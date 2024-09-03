c0e5a9fedea8a223beb9663dc25801b87b188fc5
Vinicius Santana <vsviniciuslima@usp.br>
2024-09-02 22:22:13 -0300

    Initial commit from Create Next App

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,21 @@
+import { AppSidebar } from "@/components/app-sidebar"
+import {
+  SidebarLayout,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+export default async function Page() {
+  const { cookies } = await import("next/headers")
+  return (
+    <SidebarLayout
+      defaultOpen={cookies().get("sidebar:state")?.value === "true"}
+    >
+      <AppSidebar />
+      <main className="flex flex-1 flex-col p-2 transition-all duration-300 ease-in-out">
+        <div className="h-full rounded-md border-2 border-dashed p-2">
+          <SidebarTrigger />
+        </div>
+      </main>
+    </SidebarLayout>
+  )
+}
